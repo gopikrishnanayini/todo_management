fdb9e8096650775d85d176fd53202018f1e42723
gopikrishnanayini <nayinigopi93@gmail.com>
2018-05-05 17:59:15 +0530

    changes in todos list

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,6 @@
 class Todo < ApplicationRecord
+  has_attached_file :image, styles: { medium: "300x300", thumb: "100x100" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  validates_attachment_file_name :image, matches: [/png\Z/, /jpe?g\Z/]
+  do_not_validate_attachment_file_type :image
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,6 @@ class User < ActiveRecord::Base
 
   has_attached_file :image, styles: { medium: "300x300", thumb: "100x100" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
- # validate :password_complexity
   validates_attachment_file_name :image, matches: [/png\Z/, /jpe?g\Z/]
   do_not_validate_attachment_file_type :image
-  validates :phone, :format => { :with => /\^\+[0-9]{1,3}\.[0-9]{4,14}(?:x.+)?$/, :message => "Not a valid 10-digit telephone number" }  
-  # def password_complexity
-  #   binding.pry
-  #   if password.present? and not password.match(/^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+]{8,20} /)
-  #     errors.add :password, "must include at least one uppercase letter, and one special character"
-  #   end
-  # end
 end           
