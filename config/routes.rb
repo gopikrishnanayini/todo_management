07737d884ce94722095c83e450526d0ed0457bff
gopikrishnanayini <nayinigopi93@gmail.com>
2018-05-06 23:45:48 +0530

    apis added

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,11 @@ Rails.application.routes.draw do
   root "homes#index"
   resources :todos
   resources :homes
+  namespace :api do
+    namespace :v4 do
+      resources :sessions, only: [:create, :show]
+      resources :users, only: [:index, :create, :show, :update, :destroy] 
+      resources :todos, only: [:index, :create, :show, :update, :destroy]
+    end
+  end
 end
