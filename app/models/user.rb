344752574d0226102e46f9c376801fb319c66883
gopikrishnanayini <nayinigopi93@gmail.com>
2018-05-09 09:11:35 +0530

    form channges added

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "50x50#" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   validates :password , :format => {:with => /\A(?=.*[a-zA-Z])(?=.*[0-9]).{8,}\z/, message: "must be at least 8 characters and include one number and one letter."}
-  validates :username, :phone, :presence => true
+  validates :phone, :presence => true
   validates_format_of :phone, :with =>  /\d[0-9]\)*\z/ , :message => "Only positive number without spaces are allowed"
   validates :phone, :numericality => true, :length => { :minimum => 10, :maximum => 11 }
 
