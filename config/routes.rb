ee3ef791db73552e3d0016e369771004d2ef0f40
gopikrishnnayini <nayinigopi93@gmail.com>
2018-05-09 18:29:40 +0530

    sign up issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations'
-  }
+  devise_for :users
 
 
   root "homes#index"
